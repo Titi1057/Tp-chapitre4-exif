67d875d67503962b6d43fe4eb901b663198c247f
unknown <Titi1057>
2026-06-18 20:22:09 +0300

    V3.0 - Version finale optimisée pour le rendu

--- mon_app.py
+++ mon_app.py
@@ -3,7 +3,7 @@
 #                 l'extraction exhaustive de métadonnées EXIF, l'édition de tags  
 #                 (Artiste, GPS) via un formulaire dynamique et la visualisation  
 #                 cartographique interactive (Localisation & POI)."" 
-# Nom prénom : Tchenkaeva Iman
+
 # Version ..... : V3.0  Version finale optimisée pour le rendu  
 # Environnement : Windows - VS Code - Python 3.12+  
 # Librairies .. : Streamlit, Pillow (PIL), Piexif, Folium, Streamlit-Folium, base64 
